19822c26cffd4934278d5e7f79825af26251226b
7amzawey <mohamedhamza123467890@gmail.com>
2023-07-18 21:18:12 +0300

    yes

--- 0-putchar.c
+++ 0-putchar.c
@@ -1,10 +1,12 @@
-include "main.h"
-/*
- * main check the code
- * 
+#include "main.h"
+/**
+ * main - check the code
+ *
+ *
  * Return: Always 0.
  */
-int main(void) {
+int main(void)
+{
 	putchar('_');
 	putchar('p');
 	putchar('u');

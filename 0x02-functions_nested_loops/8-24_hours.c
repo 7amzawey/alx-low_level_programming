09616dd858218bab3448650be4ef302b392d6c60
7amzawey <mohamedhamza123467890@gmail.com>
2023-07-19 20:18:19 +0300

    ..

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * jack_bauer - write evey minute of the day
+ *
+ */
+void jack_bauer(void)
+{int i;
+int j;
+	for (i = 0; i < 24; i++)
+	{
+		for (j = 0; j < 60; j++)
+		{
+			_putchar('0' + i / 10);
+			_putchar('0' + i % 10);
+			_putchar(':');
+			_putchar('0' + j / 10);
+			_putchar('0' + j % 10);
+			_putchar('\n');
+		}
+	}
+}

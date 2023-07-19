272b5a33dddeb2c764c87cf44bb24ac7068ea9c3
7amzawey <mohamedhamza123467890@gmail.com>
2023-07-19 21:47:52 +0300

    ,,

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * time_table - prints the times table
+ */
+void times_table(void)
+{int i, j, s;
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j < 10; j++)
+{
+s = i * j;
+if (s < 10)
+{
+if (j != 0)
+{
+_putchar(' ');
+}
+_putchar('0' + s);										}
+else
+{
+_putchar('0' + s / 10);
+_putchar('0' + s % 10);
+}
+if (j != 9)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+_putchar('\n');
+}
+}

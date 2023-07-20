710573c10defd23828e0d0b037faa6c416838e6d
7amzawey <mohamedhamza123467890@gmail.com>
2023-07-20 16:26:44 +0300

    ll

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_line - prints a straight line
+ * @n: the variable
+ */
+void print_line(int n)
+{int i;
+		if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+		_putchar('_');
+		}
+	}
+		_putchar('\n');
+}	

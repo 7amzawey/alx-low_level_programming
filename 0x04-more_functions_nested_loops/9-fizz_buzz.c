acb1fb9780d7ec9634334b29d2ac6c7e934233d6
7amzawey <mohamedhamza123467890@gmail.com>
2023-07-20 21:52:34 +0300

    kk

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- *  * main - solves the FizzBuzz problem
- *   * Return: always 0.
- *    */
+ * main - solves the FizzBuzz problem
+ * Return: always 0.
+ */
 int main(void)
 {int i;
 	for (i = 1; i <= 100; i++)

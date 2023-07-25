9f3b41e2f0b3eb7b7704abec01222f88710f2ab5
7amzawey <mohamedhamza123467890@gmail.com>
2023-07-26 00:27:51 +0300

    i fucken did ti :

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,12 +1,19 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * print_array - it prints the array elments
+ * @a: is the pointer
+ * @n: is the number of the elements
+ * Return: the array elements
+ */
 void print_array(int *a, int n)
 {
 	int i = *a;
-	for (i = 0; i <= n; i++)
+
+	for (i = 0; i < n; i++)
 	{
 		printf("%d", a[i]);
-		if (i < n)
+		if (i < n - 1)
 		{
 			putchar(',');
 			putchar(' ');

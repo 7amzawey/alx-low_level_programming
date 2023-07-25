c337c85d986b93388396918d6fbb8278384778d4
7amzawey <mohamedhamza123467890@gmail.com>
2023-07-26 02:16:09 +0300

    yes :

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * _atoi - converts the string to integers
+ * @s: the string
+ * Return: the integers the will represnt instead of string
+ */
+int _atoi(char *s)
+{
+int result = 0;
+int sign = 1;
+int i = 0;
+
+/* Find the first digit or sign */
+while (s[i] != '\0' && (s[i] < '0' || s[i] > '9') && s[i] != '+' && s[i] != '-')
+{
+i++;
+}
+/* Check if the first character is a sign */
+if (s[i] == '-')
+{
+sign = -1;
+i++;
+}
+else if (s[i] == '+')
+{
+i++;
+}
+/* Convert the subsequent characters to an integer */
+while (s[i] >= '0' && s[i] <= '9')
+{
+result = result * 10 + (s[i] - '0');
+i++;
+}
+result *= sign;
+return (result);
+}
+

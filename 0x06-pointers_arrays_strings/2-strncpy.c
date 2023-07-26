5efc88a02bde67756da67984794388c4a5b26457
7amzawey <mohamedhamza123467890@gmail.com>
2023-07-26 15:10:46 +0300

    the first step

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * *_strncpy - this function copies a string
+ * @dest: one of the strings
+ * @src: is the second string
+ * @n: is the length of the string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int i = 0;
+	if (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
+		

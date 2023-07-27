b3e5549dbe8a52e156e1d439cd264d019a8d670d
7amzawey <mohamedhamza123467890@gmail.com>
2023-07-27 18:56:59 +0300

    i just wanna have a good night

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * *rot13 - is a function that encodes a string using rot 13
+ * @s: is an array of chars
+ * Return: the encoded string
+ */
+char *rot13(char *s)
+{
+	int i = 0;
+	char input[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+		char output[] = "NOPQRSTUVWXYZABCDEFGHIGKLMnopqrstuvwxyzabcdefghigklm";
+	int j = 0;
+	while (s[i] != '\0')
+	{
+		for (j = 0; j < 52; j++)
+		{
+		if(s[i] == input[j])
+		{
+		s[i] = output[j];
+		break;
+		}
+		}
+		i++;
+	}
+	return (s);
+}

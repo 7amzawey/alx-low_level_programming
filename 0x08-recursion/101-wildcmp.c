a455229a09dfac4e418f99ebde18ddbe2f705ded
7amzawey <mohamedhamza123467890@gmail.com>
2023-09-29 21:39:23 +0300

    hell no

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -8,8 +8,7 @@
 
 int _strlen(char *s)
 {	int length = 0;
-	
-	if(*s != '\0')
+	if (*s != '\0')
 	{
 		length++;
 		s++;
@@ -25,12 +24,11 @@ int _strlen(char *s)
  */
 int helper(char *s1, char *s2, int i)
 {
-	
-	if(s1[i] != s2[i])
+	if (s1[i] != s2[i])
 	{
-	return 0;
+		return (0);
 	}
-	else if( s1[i] == '\0')
+	else if (s1[i] == '\0')
 	{
 		return (1);
 	}
@@ -47,13 +45,11 @@ int helper(char *s1, char *s2, int i)
  * @s2: is the second string
  * Return: 1 if they are identic 0 if not
  */
-	
 int wildcmp(char *s1, char *s2)
 {
-
 	if (_strlen(s1) != _strlen(s2))
 	{
-		return(0);
+		return (0);
 	}
 	return (helper(s1, s2, 0));
 }

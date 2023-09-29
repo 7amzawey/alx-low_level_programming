bb32892538c0730af0c5b01387a731724db12521
7amzawey <mohamedhamza123467890@gmail.com>
2023-09-29 17:29:43 +0300

    noway

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * helper - is here to help idiot
+ * @n: is the bloody number at the function
+ * @i: this a helper as well
+ * Return: the square root
+ */
+int helper(int n, int i)
+{
+	if (i * i > n)
+	{
+		return (-1);
+	}
+	else if (i * i == n)
+	{
+		return (i);
+	}
+	return (helper(n, i + 1));
+}
+/**
+ * _sqrt_recursion - returns the square route
+ * @n: the number
+ * Return: the square route
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		{
+			return (-1);
+		}
+	return (helper(n, 0));
+}

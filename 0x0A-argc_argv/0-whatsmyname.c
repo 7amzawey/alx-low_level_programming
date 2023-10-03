adf02f2b4f6e3a4e6238587edaf690383bb07ee4
7amzawey <mohamedhamza123467890@gmail.com>
2023-10-03 12:16:08 +0300

    the 1st is done

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,13 +4,15 @@
  * main - the entry point
  * @argc: the count of strings
  * @argv: the array of strings
+ * Return: the name
  */
-int main(int argc,char* argv[])
+int main(int argc, char *argv[])
 {
 int i;
+
 for (i = 0; i < argc; i++)
 {
-	printf("%s\n",argv[i]);
+	printf("%s\n", argv[i]);
 
 }
 return (0);

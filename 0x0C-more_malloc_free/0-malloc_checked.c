a84d57217d056108f4d46f1acba4ac946cf8ffdf
7amzawey <mohamedhamza123467890@gmail.com>
2023-10-08 10:31:11 +0300

    0k

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * *malloc_checked - allocates memory using malloc
+ * @b: is the size.
+ * returns a pointer to allocated 
+ * memory
+ * if malloc fails it causes a mormal proccess 
+ * termination with a status value of 98
+ */
+void *malloc_checked(unsigned int b)
+{
+	char *ptr = malloc(sizeof(char) * b);
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	return ptr;
+}

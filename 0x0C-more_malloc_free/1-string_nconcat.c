4fd0d85b038f2204d8574d7c5dbef129d6fe5a74
7amzawey <mohamedhamza123467890@gmail.com>
2023-10-08 11:20:16 +0300

    lets see

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,4 +10,45 @@
  * if null passed treat it as an empty string
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
-{
+{	int size = 0;
+	int i;
+	unsigned int j;
+	unsigned int len2 = 0;
+
+	if(s1 == NULL)
+	{
+		s1 = "";
+	}
+	if(s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	while(s1[size])
+	{
+		size++;
+	}
+	while(s2[size] && len2 < n)
+	{
+		len2++;
+	}
+	char *ptr = malloc(sizeof(char) * (size + n + 1));
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0;i < size; i++)
+	{
+	ptr[i] = s1[i];
+	}
+	for (j = 0; j < len2; j++)
+	{
+	ptr[j + i] = s2[j];
+	}
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	ptr[j + i] = '\0';
+	return (ptr);
+}	

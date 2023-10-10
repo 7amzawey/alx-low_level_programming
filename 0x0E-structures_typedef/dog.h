bb5487e83e95670cf95bc4b747c985d875398e1b
7amzawey <mohamedhamza123467890@gmail.com>
2023-10-10 18:08:06 +0300

    the first is bloody done

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - is the dam dog
+ * @name: the dog name
+ * @age: the dog name
+ * @owner: the owner name
+ * Description: it is just a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif

c6c4f2f69e8ef1286809090f14c1ab96db2c327e
7amzawey <mohamedhamza123467890@gmail.com>
2023-10-11 22:26:19 +0300

    what the hill was that

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+ * op_add - sum of two integers
+ * @a: is the 1st integer
+ * @b: is the 2nd integer
+ * Return: the sum
+ */
+int op_add(int a, int b)
+{
+		return (a + b);
+}
+/**
+ * op_sub - the sub of two integers
+ * @a: is the first int
+ * @b: is the second
+ * Return: the sub
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_div - the diviation of two integers
+ * @a: is the first
+ * @b: is the second
+ * Return: the divaiton
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mul - the multi of two integers
+ * @a: the 1st
+ * @b: the 2nd
+ * Return: the mul
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_mod - returns the reminder of the divison
+ * @a: the 1st
+ * @b: the 2nd
+ * Return: the modulous
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}

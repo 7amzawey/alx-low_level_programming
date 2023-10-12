1075bc3fe7d10c65a774d47ff6c2ff696b2035d9
7amzawey <mohamedhamza123467890@gmail.com>
2023-10-12 20:23:59 +0300

    cool

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,7 +1,8 @@
 #include "variadic_functions.h"
 /**
- * str - the bloody structure
- * Description: dheo
+ * struct str - the bloody structure
+ * @type_id: the type identifier
+ * @print_func: the print function
  */
 struct str
 {
@@ -10,45 +11,53 @@ void (*print_func)(va_list);
 };
 /**
  * print_char - prints char
+ * @last: is the last
+ * @args: kk
  */
-void print_char(va_list args)
+void print_char(va_list args, int last)
 {
 	char c = va_arg(args, int);
 
-	printf("%c", c);
+	printf("%c%s", c, last ? "\n" : ", ");
 }
 /**
  * print_string - prints a bloody string
+ * @last: is the last
+ * @args: kk
  */
-void print_string(va_list args)
+void print_string(va_list args , int last)
 {
 	char *s = va_arg(args, char*);
 
 	if (s == NULL)
 		printf("(nil)");
 	else
-		printf("%s", s);
+		printf("%s%s", s, last ? "\n" : ", ");
 }
 /**
  * print_int - prints a bloody int
+ * @last: is the last
+ * @args: kk
  */
-void print_int(va_list args)
+void print_int(va_list args, int last)
 {
 	int i = va_arg(args, int);
 
-	printf("%d", i);
+	printf("%d%s", i, last ? "\n" : ", ");
 }
 /**
  * print_float - prints a bloody float
+ * @last: is the last
+ * @args: KK
  */
-void print_float(va_list args)
+void print_float(va_list args, int last)
 {
 	double f = va_arg(args, double);
 
-	printf("%f", f);
+	printf("%f%s", f, last ? "\n" : ", ");
 }
 /**
- * str print_func[] - an array of sturcts
+ * stuct str print_funcs - cool
  */
 struct str print_funcs[] = {
 {'c', print_char},

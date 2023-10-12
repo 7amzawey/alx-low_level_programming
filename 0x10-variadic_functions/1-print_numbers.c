2733b1bccfe48b337428945bffafa4e49688f700
7amzawey <mohamedhamza123467890@gmail.com>
2023-10-12 15:46:10 +0300

    1-print_numbers.c is cool

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,21 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - that will print numbers broooh
+ * @separator - is the string to be printed between 
+ * numbers
+ * @n: is the number of numbers to be printed
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list mo;
+	va_start(mo, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(mo, int));
+		if(i != n-1)
+		printf("%s", separator);
+	}
+	printf("\n");
+}

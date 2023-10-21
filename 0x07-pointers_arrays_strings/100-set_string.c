cba3632209da644a3eae222663fc1e9e0f8be90f
7amzawey <mohamedhamza123467890@gmail.com>
2023-10-21 16:02:09 +0300

    because am faid it money in the bank all the money all we need it

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * set_string - the functions sets the value of a
+ * pointer to a char
+ * @s: the pointer
+ * @to: is the char
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}

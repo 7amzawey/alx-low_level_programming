5bef98d0f64c8a137c9a033ad3e6ed5f8c44fdd5
7amzawey <mohamedhamza123467890@gmail.com>
2023-10-21 20:15:40 +0300

    cool

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,44 +1,33 @@
 #include "main.h"
-
-/**
- * _strlen - gets the string length
- * @s: is the string
- * Return: 1 if they are identic 0 if not
- */
-
-int _strlen(char *s)
-{	int length = 0;
-	if (*s != '\0')
-	{
-		length++;
-		s++;
-	}
-	return (length);
-}
 /**
  * helper - just for help
  * @s1: the first string
  * @s2: the second string
- * @i: just an integer
  * Return: 1 if identical 0 if not
  */
-int helper(char *s1, char *s2, int i)
+int helper(char *s1, char *s2)
 {
-	if (s1[i] != s2[i])
-	{
-		return (0);
-	}
-	else if (s1[i] == '\0')
+	if (*s1 == '\0' && *s2 == '\0')
 	{
 		return (1);
 	}
+	else if (*s2 == '*')
+	{
+		if (*s1 != '\0' && helper(s1 + 1, s2))
+		{
+		return (1);
+		}
+		return (helper(s1, s2 + 1));
+	}
+	else if (*s1 != *s2)
+	{
+		return (0);
+	}
 	else
 	{
-	return (helper(s1, s2, i + 1));
+		return (helper(s1 + 1, s2 + 1));
 	}
 }
-
-
 /**
  * wildcmp - compares to funcs
  * @s1: is the first string
@@ -47,9 +36,5 @@ int helper(char *s1, char *s2, int i)
  */
 int wildcmp(char *s1, char *s2)
 {
-	if (_strlen(s1) != _strlen(s2))
-	{
-		return (0);
-	}
-	return (helper(s1, s2, 0));
+	return (helper(s1, s2));
 }

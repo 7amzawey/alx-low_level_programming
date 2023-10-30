2079dfa8e2b26cdc3e3a3c8bf52ba4fefcbba13d
7amzawey <mohamedhamza123467890@gmail.com>
2023-10-30 16:55:17 +0200

    fine

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ * _strlen - returns the len of string
+ * @str: the string
+ * Return: the string lenght
+ */
+int _strlen(char *str)
+{
+	int len = 0;
+	if (!str)
+	return (0);
+	while (*str++)
+	{
+		len++;
+	}
+	return (len);
+}
+/**
+ * create_file _ creates a function that creats file
+ * @filename: is the name of the file to be created
+ * @text_content: is a NULL terminated string to
+ * write to the file.
+ * Return: 1 on success , -1 on filure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int bytes = 0;
+	int len;
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC , S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
+	len = _strlen(text_content);
+
+	if (len)
+	bytes = write(fd, text_content, len);
+	close(fd);
+	return (bytes == len ? 1 : -1);
+}

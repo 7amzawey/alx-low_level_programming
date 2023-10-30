ba964f0162a9cbf083d9de4a4ffb63f073c4a513
7amzawey <mohamedhamza123467890@gmail.com>
2023-10-30 16:59:19 +0200

    2nd task

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,6 +7,7 @@
 int _strlen(char *str)
 {
 	int len = 0;
+
 	if (!str)
 	return (0);
 	while (*str++)
@@ -16,7 +17,7 @@ int _strlen(char *str)
 	return (len);
 }
 /**
- * create_file _ creates a function that creats file
+ * create_file - creates a function that creats file
  * @filename: is the name of the file to be created
  * @text_content: is a NULL terminated string to
  * write to the file.
@@ -30,7 +31,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC , S_IRUSR | S_IWUSR);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
 	len = _strlen(text_content);

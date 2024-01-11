afa350093c755802007ae6699f399e627cf1dbe0
7amzawey <mohamedhamza123467890@gmail.com>
2024-01-11 16:33:36 +0200

    fine

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,13 +11,18 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int i;
 	dlistint_t *new = malloc(sizeof(dlistint_t));
 	dlistint_t *head = *h;
-	
+
 	if (!h || !new)
 	{
 		free(new);
 		return (NULL);
 	}
 	new->n = n;
+	if (idx == 0)
+	{
+		new = add_dnodeint(h, n);
+		return (new);
+	}
 	for (i = 0; i < idx - 1 && head != NULL; i++)
 	{
 		head = head->next;
@@ -33,7 +38,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			if (head->next != NULL)
 				head->next->prev = new;
 			head->next = new;
-		
+
 		head = head->next;
 	return (new);
 }

2d8e1d20013269aa402347932b58b9f1c53ae93d
7amzawey <mohamedhamza123467890@gmail.com>
2024-01-11 20:31:53 +0200

    some error handling

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * size_t print_dlistint - writes a functions that prints all elements of
+ * print_dlistint - writes a functions that prints all elements of
  * a list
  * @h: this is the header node
  * Return: the number of node

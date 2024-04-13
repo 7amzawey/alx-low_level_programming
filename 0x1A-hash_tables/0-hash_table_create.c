23fb8f33b18ecf344eb4744e31092ff72774e51e
7amzawey <mohamedhamza123467890@gmail.com>
2024-04-13 13:40:53 +0200

    some updates

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,7 +11,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int i;
 
 	if (!table)
+	{
+		free (table);
 		return (NULL);
+	}
 	table->size = size;
 	table->array = malloc(sizeof(hash_node_t *) * size);
 

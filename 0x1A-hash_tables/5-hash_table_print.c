3136010c0b0da8e3f22df0280b2904a0b30d8daf
7amzawey <mohamedhamza123467890@gmail.com>
2024-04-14 09:29:56 +0200

    print

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - prints a hash table
+ * @ht: is the hash table.
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	int printed;
+
+	printf("{");
+
+	if (ht)
+	{
+		for (i = 0; i < ht->size; i++)
+		{
+			hash_node_t *current_node = ht->array[i];
+
+			while (current_node != NULL)
+			{
+				if (printed == 1)
+					printf(", ");
+
+				printf("'%s' : '%s'", current_node->key, current_node->value);
+				printed = 1;
+				current_node = current_node->next;
+			}
+		}
+	}
+	printf("}\n");
+}

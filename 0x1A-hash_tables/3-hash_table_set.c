0a0c24497fdf5399e44072717cda2c1496a75959
7amzawey <mohamedhamza123467890@gmail.com>
2024-04-14 11:29:39 +0200

    some updates

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,17 +9,29 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int x;
-	hash_node_t *new_node;
+	hash_node_t *new_node, *node;
 
 	if (!key || !ht || !value)
 		return (0);
+
+	x = key_index((const unsigned char *)key, ht->size);
+	node = ht->array[x];
+	
+	while(node)
+	{
+		if (strcmp(node->key,key) == 0)
+		{
+			free(node->value);
+			node->value = strdup(value);
+			return (1);
+		}
+		node = node->next;
+	}
+
 	new_node = malloc(sizeof(hash_node_t));
 
 	if (!new_node)
-	{
-		free(new_node);
 		return (0);
-	}
 
 	new_node->key = strdup(key);
 
@@ -36,7 +48,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(new_node);
 		return (0);
 	}
-	x = key_index((const unsigned char *)key, ht->size);
 	new_node->next = ht->array[x];
 	ht->array[x] = new_node;
 	return (1);

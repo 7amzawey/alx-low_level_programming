b218f61394873eb954adfe52dbdf5cb88663cacf
7amzawey <mohamedhamza123467890@gmail.com>
2024-06-02 22:13:26 +0200

    fine

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -2,22 +2,26 @@
 """
 this module is for defining the island_perimeter function
 """
+
+
 def island_perimeter(grid):
-    """this function will calculate the perimeter of the island which is 
+    """this function will calculate the perimeter of the island which is
     represneted by ones
     """
     per = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
-                if j == len(grid[i]) - 1 or (j + 1 < len(grid[i]) and grid[i][j + 1] == 0):
+                if j == len(grid[i]) - 1 or (
+                        j + 1 < len(grid[i]) and grid[i][j + 1] == 0):
                     per += 1
                 if j == 0 or grid[i][j - 1] == 0:
                     per += 1
                 if i == 0 or grid[i - 1][j] == 0:
                     per += 1
-                if i == len(grid) - 1 or (i + 1 < len(grid) and grid[i + 1][j] == 0):
-                     per += 1
+                if i == len(grid) - 1 or (
+                        i + 1 < len(grid) and grid[i + 1][j] == 0):
+                    per += 1
             else:
                 pass
     return (per)

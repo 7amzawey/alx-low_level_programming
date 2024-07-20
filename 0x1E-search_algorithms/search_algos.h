249384affb46669d44baabf47829c792510b8461
7amzawey <mohamedhamza123467890@gmail.com>
2024-07-20 13:26:49 +0200

    betty fixing

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -15,10 +15,10 @@
  */
 typedef struct skiplist_s
 {
-    int n;
-    size_t index;
-    struct skiplist_s *next;
-    struct skiplist_s *express;
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
 } skiplist_t;
 skiplist_t *linear_skip(skiplist_t *list, int value);
 /**
@@ -32,9 +32,9 @@ skiplist_t *linear_skip(skiplist_t *list, int value);
  */
 typedef struct listint_s
 {
-    int n;
-    size_t index;
-    struct listint_s *next;
+	int n;
+	size_t index;
+	struct listint_s *next;
 } listint_t;
 listint_t *jump_list(listint_t *list, size_t size, int value);
 int compare(const void *a, const void *b);
